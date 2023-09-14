089e3bbea29a31f4df65f7051facad83e55616a0
Drew Lytle <aml3ed@virginia.edu>
2023-09-14 11:39:29 -0400

    Add typechecking env variables

---
+++ src/env.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+  GITHUB_TOKEN: z.string().nonempty(),
+  GITHUB_REPO_OWNER: z.string().nonempty(),
+  GITHUB_REPO_NAME: z.string().nonempty(),
+});
+
+export const env = envSchema.parse(Bun.env);
