b51907345dead763c33e8a75927ce4e6686b031f
Drew Lytle <aml3ed@virginia.edu>
2023-12-15 15:45:39 -0500

    Add alpha redirect

---
+++ src/pages/alpha.ts
@@ -0,0 +1,7 @@
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async ({ request }) => {
+  return Response.redirect(
+    "https://curved-dogsled-98c.notion.site/Repbot-Alpha-Onboarding-498e97e99fe94dd1bbb96e5091d37b5a",
+  );
+};
